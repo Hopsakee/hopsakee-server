047734cf0a60f0b04ff297ab84974de3601aea16
Solveit <nobody@fast.ai>
2026-04-12 14:17:40 +0000

    renamed deploy scripts, added pushing of secrets from client to server

---
+++ server_setup/client-deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Run from your local machine to push secrets and deploy all apps on the server.
+
+set -a
+source "$(dirname "${BASH_SOURCE[0]}")/../.env"
+set +a
+
+SERVER="ubuntu@hopsakee.top"
+SSH="ssh -i ~/.ssh/tps_si"
+SECRETS_DIR="/data/authelia/secrets"
+
+echo ">>> Creating secrets directory on server..."
+$SSH $SERVER "mkdir -p $SECRETS_DIR"
+
+echo ">>> Pushing persistent secrets..."
+$SSH $SERVER "echo -n '$AUTHELIA_STORAGE_ENCRYPTION_KEY' > $SECRETS_DIR/storage_encryption_key"
+$SSH $SERVER "echo -n '$AUTHELIA_SMTP_USERNAME' > $SECRETS_DIR/smtp_username"
+$SSH $SERVER "echo -n '$AUTHELIA_SMTP_PASSWORD' > $SECRETS_DIR/smtp_password"
+
+echo ">>> Generating fresh jwt and session secrets..."
+$SSH $SERVER "python3 -c \"import secrets; open('$SECRETS_DIR/jwt_secret','w').write(secrets.token_hex(32))\""
+$SSH $SERVER "python3 -c \"import secrets; open('$SECRETS_DIR/session_secret','w').write(secrets.token_hex(32))\""
+
+echo ">>> Pulling latest git changes and deploying..."
+$SSH $SERVER "cd ~/hopsakee-server && git pull && bash ~/hopsakee-server/server_setup/server-deploy.sh"
--- server_setup/redeploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Run from your local machine to pull latest git changes and redeploy all apps on the server.
-
-ssh -i ~/.ssh/tps_si ubuntu@hopsakee.top '
-  cd ~/hopsakee-server && git pull
-  bash ~/hopsakee-server/server_setup/deploy.sh
-'
